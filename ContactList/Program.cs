ba2c86e4cb76b31966f252d2f66f1d171a062f2b
RichRavenM <richard.s.morris@btinternet.com>
2024-08-18 11:44:22 +0100

    notes for Amier

--- ContactList/Program.cs
+++ ContactList/Program.cs
@@ -11,7 +11,7 @@ namespace ContactList
     {
         static void Main(string[] args)
         {
-            
+
             ContactManager contactManager = new ContactManager();
 
             while (true)
@@ -20,14 +20,15 @@ namespace ContactList
                 Console.WriteLine("2: View contacts");
                 Console.WriteLine("3: Edit contacts");
                 Console.WriteLine("4: Delete contacts");
+                //might be worth adding a view contact (singular) option
 
-                var option = Console.ReadLine();    
+                var option = Console.ReadLine();
 
-                switch(option)
+                switch (option)
                 {
                     case "1":
                         AddContact(contactManager);
-                        break; 
+                        break;
                     case "2":
                         GetAllContacts(contactManager);
                         break;
@@ -39,19 +40,21 @@ namespace ContactList
                         break;
 
                 }
-            }   
+            }
         }
-
+        // this whole block below should be in its own class. Its purpose is different to the program class. Think of the below as a Service and your ContactManager class as the Repository
         private static void DeleteContact(ContactManager contactManager)
         {
             Console.WriteLine("Enter Id of contact you want to delete: ");
             var contactId = Console.ReadLine();
 
             if (int.TryParse(contactId, out int id))
-            { 
+            {
 
-            contactManager.DeleteContact(id);
+                contactManager.DeleteContact(id);
+                //You'll need some error handling for this in the case that the id doesn't exist
             }
+            //You'll want an else block for when the tryParse fails. Maybe another message
         }
 
         private static void UpdateContact(ContactManager contactManager)
@@ -65,19 +68,21 @@ namespace ContactList
 
                 if (contact != null)
 
-                Console.WriteLine("Edit Name: ");
+                    Console.WriteLine("Edit Name: ");
                 contact.Name = Console.ReadLine();
                 Console.WriteLine("Edit Number: ");
                 contact.Phone = Console.ReadLine();
 
                 contactManager.UpdateContact(contact);
             }
+
+
         }
 
         public static void AddContact(ContactManager contactManager)
         {
             Contact contact = new Contact();
-
+            //You'll probably want validation for the below fields
             Console.WriteLine("Add Contact Name");
             contact.Name = Console.ReadLine();
             Console.WriteLine("Add Phone Nummber");
@@ -85,6 +90,7 @@ namespace ContactList
 
             contactManager.AddContact(contact);
             Console.WriteLine(contact.Id);
+            // if you're going to display the id, you probably want it to be clear to the user that this is the id
         }
 
         public static void GetAllContacts(ContactManager contactManager)
@@ -93,7 +99,7 @@ namespace ContactList
 
             foreach (var contact in contacts)
             {
-                Console.WriteLine(contact.Name);
+                Console.WriteLine(contact.Name); // you probably want to include the id
             }
         }
 
